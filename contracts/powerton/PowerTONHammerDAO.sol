d0d73b205215fb0528aed5f9e360c2448eb832ab
Jeongun Baek <jubaek@enki.co.kr>
2022-06-24 17:45:55 +0900

    업그레이드를 지원하기 위한 코드 변경

--- contracts/powerton/PowerTONHammerDAO.sol
+++ contracts/powerton/PowerTONHammerDAO.sol
@@ -3,113 +3,150 @@ pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { ILockTOSDividend } from "../interfaces/ILockTOSDividend.sol";
-import { iPowerTON } from "./iPowerTON.sol";
 
+import { iPowerTON } from "./iPowerTON.sol";
 import "./IPowerTONSwapperEvent.sol";
-import "./SeigManagerI.sol";
+
+import "../interfaces/IIERC20.sol";
+import "../interfaces/IAutoCoinageSnapshot.sol";
+
 import "../common/AccessibleCommon.sol";
+import "./PowerTONSwapperStorage.sol";
+
+import { PowerTONHammerDAOStorage } from "./PowerTONHammerDAOStorage.sol";
+import { ILockTOSDividend } from "../interfaces/ILockTOSDividend.sol";
 
 contract PowerTONHammerDAO is
+    PowerTONSwapperStorage,
     AccessibleCommon,
     iPowerTON,
-    IPowerTONSwapperEvent
+    IPowerTONSwapperEvent,
+    PowerTONHammerDAOStorage
 {
-    // WTON 컨트랙트 주소
-    address public wton;
-    // sTOS 홀더에게 에어드랍하기 위한 LockTOSDividendProxy 컨트랙트
-    ILockTOSDividend public lockTOSDividendProxy;
+    modifier onlySeigManagerOrOwner() {
+        require(
+            isAdmin(msg.sender) ||
+            msg.sender == seigManager,
+            "PowerTONSwapper: sender is not seigManager or not admin");
+        _;
+    }
+
+    event OnChangeAmountInRecoder(address indexed account, uint256 amountToMint, uint256 amountToBurn);
 
     constructor()
     {
-        _setRoleAdmin(ADMIN_ROLE, ADMIN_ROLE);
-        _setupRole(ADMIN_ROLE, msg.sender);
     }
 
-    /// @dev PowerTON 컨트랙트가 동작하는데 필요한 변수들 설정
-    /// @param _wton WTON 토큰 주소
-    /// @param _lockTOSDividendProxy sTOS 홀더들에게 에어드랍하는 컨트랙트 주소
     function setInfo(
         address _wton,
-        address _lockTOSDividendProxy
+        address _autocoinageSnapshot,
+        address _seigManager,
+        address _dividiedPool
         )
         external onlyOwner
     {
         wton = _wton;
-        lockTOSDividendProxy = ILockTOSDividend(_lockTOSDividendProxy);
+        autocoinageSnapshot = _autocoinageSnapshot;
+        seigManager = _seigManager;
+        dividiedPool = ILockTOSDividend(_dividiedPool);
     }
 
-    /// @dev sTOS 홀더들에게 에어드랍하는 컨트랙트에 WTON 위임(approve)
+    function setAutocoinageSnapshot(
+        address _autocoinageSnapshot
+        )
+        external onlyOwner
+    {
+        autocoinageSnapshot = _autocoinageSnapshot;
+    }
+
+    /// @dev 업그레이드 이후 사용되지 않는 함수
+    function approveToUniswap() external {
+    }
+    
+    /// @notice PowerTON으로 쌓인 WTON 전체를 LockTOSDividendProxy 컨트랙트에 위임
     function approveToDividendPool() external {
         IERC20(wton).approve(
-            address(lockTOSDividendProxy),
+            address(dividiedPool),
             type(uint256).max
         );
     }
 
-    /// @dev PowerTON 컨트랙트에 적립된 WTON을 sTOS 홀더들에게 에어드랍하는 함수
-    /// @notice WTON을 approve 하기 위해 approveToDividendPool를 먼저 호출해야함
+    /// @notice LockTOSDividendProxy 컨트랙트를 사용해서 sTOS 홀더에게 에어드랍
     function distribute() external {
         uint256 wtonBalance = getWTONBalance();
-        lockTOSDividendProxy.distribute(wton, wtonBalance);
+        dividiedPool.distribute(wton, wtonBalance);
         emit Distributed(wton, wtonBalance);
+    }
+
+    /// @dev 업그레이드 이후 사용되지 않는 함수
+    function swap(
+        uint24 _fee,
+        uint256 _deadline,
+        uint256 _amountOutMinimum,
+        uint160 _sqrtPriceLimitX96
+    )
+        external
+    {
+        _fee;
+        _deadline;
+        _amountOutMinimum;
+        _sqrtPriceLimitX96;
+        emit Swapped(_amountOutMinimum);
     }
 
     function getWTONBalance() public view returns(uint256) {
         return IERC20(wton).balanceOf(address(this));
     }
 
-
-    /// @dev 하위 호환성을 위해 남겨진 함수 제거하면 compile 실패
+    /// @dev 업그레이드 이후 사용되지 않는 함수
     function currentRound() external override pure returns (uint256) {
         return 0;
     }
 
-    /// @dev 하위 호환성을 위해 남겨진 함수 제거하면 compile 실패
+    /// @dev 업그레이드 이후 사용되지 않는 함수
     function roundDuration() external override pure returns (uint256) {
         return 0;
     }
 
-    /// @dev 하위 호환성을 위해 남겨진 함수 제거하면 compile 실패
+    /// @dev 업그레이드 이후 사용되지 않는 함수
     function totalDeposits() external override pure returns (uint256) {
         return 0;
     }
 
-    /// @dev 하위 호환성을 위해 남겨진 함수 제거하면 compile 실패
+    /// @dev 업그레이드 이후 사용되지 않는 함수
     function winnerOf(uint256 round) external override pure returns (address) {
-        round;
         return address(0);
     }
 
-    /// @dev 하위 호환성을 위해 남겨진 함수 제거하면 compile 실패
+    /// @dev 업그레이드 이후 사용되지 않는 함수
     function powerOf(address account) external override pure returns (uint256) {
-        account;
         return 0;
     }
 
-    /// @dev 하위 호환성을 위해 남겨진 함수 제거하면 compile 실패
+    /// @dev 업그레이드 이후 사용되지 않는 함수
     function init() external override {
     }
 
-    /// @dev 하위 호환성을 위해 남겨진 함수 제거하면 compile 실패
+    /// @dev 업그레이드 이후 사용되지 않는 함수
     function start() external override {
     }
 
-    /// @dev 하위 호환성을 위해 남겨진 함수 제거하면 compile 실패
+    /// @dev 업그레이드 이후 사용되지 않는 함수
     function endRound() external override {
     }
 
-    /// @dev 하위 호환성을 위해 남겨진 함수 제거하면 compile 실패
     function onDeposit(address layer2, address account, uint256 amount)
-        external override onlyOwner
+        external override onlySeigManagerOrOwner
     {
-        emit OnDeposit(layer2, account, amount);
+        IAutoCoinageSnapshot(autocoinageSnapshot).addSync(layer2, account);
+        //emit OnDeposit(layer2, account, amount);
     }
 
-    /// @dev 하위 호환성을 위해 남겨진 함수 제거하면 compile 실패
     function onWithdraw(address layer2, address account, uint256 amount)
-        external override onlyOwner
+        external override onlySeigManagerOrOwner
     {
-        emit OnWithdraw(layer2, account, amount);
+        IAutoCoinageSnapshot(autocoinageSnapshot).addSync(layer2, account);
+        //emit OnWithdraw(layer2, account, amount);
     }
+
 }
--- tasks/check-powerton-hammer-dao.js
+++ tasks/check-powerton-hammer-dao.js
@@ -23,41 +23,57 @@ task('check-powerton-hammer-dao').setAction(async () => {
   const ABI = [
     "function setPowerTON(address)",
     "function updateSeigniorage()",
-    "function claim(address)"
+    "function claim(address)",
+    "function setInfo(address,address,address,address)",
+    "function approveToDividendPool()",
+    "function distribute()",
+    "function upgradeTo(address)"
   ]
 
   const WTON = "0xc4A11aaf6ea915Ed7Ac194161d2fC9384F15bff2";
-  const SeigManagerOwner = "0x15280a52e79fd4ab35f4b9acbb376dcd72b44fd1";
-  const SeigManagerAddress = "0x710936500aC59e8551331871Cbad3D33d5e0D909";
-  const OPERATOR = "0x5d9a0646c46245a8a3b4775afb3c54d07bcb1764";
+  const SEIG_MANAGER_ONWER = "0x15280a52e79fd4ab35f4b9acbb376dcd72b44fd1";
+  const SEIG_MANAGER = "0x710936500aC59e8551331871Cbad3D33d5e0D909";
+  const AUTO_COINASGE_SNAPSHOT = "0x85ca9f611c363065252ea9462c90743922767b55";
+  const HAMMER_DAO = "0x5d9a0646c46245a8a3b4775afb3c54d07bcb1764";
   const HOLDER = "0x4D145A5e90736ad71cBF0081366625BD9eb170Cc";
-  const DIVIDENTPOOL = "0x17332F84Cc0bbaD551Cd16675F406A0a2c55E28C"
+  const DIVIDENTPOOL = "0x17332F84Cc0bbaD551Cd16675F406A0a2c55E28C";
+  const POWERTON_SWAPPER_PROXY = "0x970298189050aBd4dc4F119ccae14ee145ad9371";
+  const POWERTON_SWAPPER_PROXY_OWNER = "0x15280a52e79fd4ab35f4b9acbb376dcd72b44fd1";
   
-  const PowerTONContract = await ethers.getContractFactory("PowerTONHammerDAO");
-  const powerTon = await PowerTONContract.deploy();
+  const powerTONContract = await ethers.getContractFactory("PowerTONHammerDAO");
+  const powerTon = await powerTONContract.deploy();
   await powerTon.deployed();
 
-  await powerTon.setInfo(WTON, DIVIDENTPOOL);
+  await network.provider.send('hardhat_setBalance', [
+    POWERTON_SWAPPER_PROXY_OWNER,
+    hex(parseEth(10000)),
+  ]);
+
+  start_impersonate(POWERTON_SWAPPER_PROXY_OWNER);
+  const powerTONProxy = await ethers.getContractAt(ABI, POWERTON_SWAPPER_PROXY, await ethers.getSigner(POWERTON_SWAPPER_PROXY_OWNER));
+  await powerTONProxy.upgradeTo(powerTon.address);
+
+  await powerTONProxy.setInfo(WTON, AUTO_COINASGE_SNAPSHOT, SEIG_MANAGER, DIVIDENTPOOL);
 
   console.log(`PowerTON : ${powerTon.address}`);
 
-  start_impersonate(SeigManagerOwner);
-  await network.provider.send('hardhat_setBalance', [SeigManagerOwner, hex(parseEth(10000))]);
-  const seigManager = await ethers.getContractAt(ABI, SeigManagerAddress, await ethers.getSigner(SeigManagerOwner));
+  start_impersonate(SEIG_MANAGER_ONWER);
+  await network.provider.send('hardhat_setBalance', [SEIG_MANAGER_ONWER, hex(parseEth(10000))]);
+  const seigManager = await ethers.getContractAt(ABI, SEIG_MANAGER, await ethers.getSigner(SEIG_MANAGER_ONWER));
 
   await seigManager.setPowerTON(powerTon.address);
 
   mining(0x100000);
 
-  const operator = await ethers.getContractAt(ABI, OPERATOR);
+  const operator = await ethers.getContractAt(ABI, HAMMER_DAO);
   await operator.updateSeigniorage();
 
   const wton = await ethers.getContractAt("IERC20", WTON);
   console.log(`PowerTON's WTON Balance ${await wton.balanceOf(powerTon.address) / 1e27}`);
 
   console.log(`[BEFORE] sTOS holder's WTON Balance ${await wton.balanceOf(HOLDER) / 1e27}`);
-  await powerTon.approveToDividendPool();
-  await powerTon.distribute();
+  await powerTONProxy.approveToDividendPool();
+  await powerTONProxy.distribute();
 
   mining(0x100000);
   
